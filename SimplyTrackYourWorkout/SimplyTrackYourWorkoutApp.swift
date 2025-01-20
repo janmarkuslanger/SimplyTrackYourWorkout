db038bf7659d17518686cc370bb7ab1c18b7d643
janmarkuslanger <janmarkuslanger10121994@gmail.com>
2025-01-20 18:39:52 +0100

    init

---
+++ SimplyTrackYourWorkout/SimplyTrackYourWorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplyTrackYourWorkoutApp.swift
+//  SimplyTrackYourWorkout
+//
+//  Created by Jan-Markus Langer on 20.01.25.
+//
+
+import SwiftUI
+
+@main
+struct SimplyTrackYourWorkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SimplyTrackYourWorkoutTests/SimplyTrackYourWorkoutTests.swift
@@ -0,0 +1,17 @@
+//
+//  SimplyTrackYourWorkoutTests.swift
+//  SimplyTrackYourWorkoutTests
+//
+//  Created by Jan-Markus Langer on 20.01.25.
+//
+
+import Testing
+@testable import SimplyTrackYourWorkout
+
+struct SimplyTrackYourWorkoutTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
