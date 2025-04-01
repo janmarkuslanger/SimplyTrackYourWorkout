01c809f4cd310771160768dd28945412864da3b1
janmarkuslanger <janmarkuslanger10121994@gmail.com>
2025-04-01 20:49:37 +0200

    clean up

--- SimplyTrackYourWorkout/Views/EditTemplateExerciseView.swift
+++
@@ -1,7 +0,0 @@
-//
-//  EditTemplateExerciseView.swift
-//  SimplyTrackYourWorkout
-//
-//  Created by Jan-Markus Langer on 14.03.25.
-//
-
--- SimplyTrackYourWorkout/Views/HomePageView.swift
+++ SimplyTrackYourWorkout/Views/HomePageView.swift
@@ -1,46 +1,13 @@
 import SwiftUI
 
 struct HomePageView: View {
-    @State private var templates: [(id: Int64, name: String)] = []
-
     var body: some View {
         NavigationView {
             VStack {
-                List {
-                    ForEach(templates, id: \.id) { template in
-                        NavigationLink(destination: TrackWorkoutView(templateID: template.id, templateName: template.name)) {
-                            Text(template.name)
-                        }
-                    }
-                }
-                .onAppear(perform: loadTemplates)
-
-                VStack(spacing: 12) {
-                    NavigationLink(destination: ManageTemplatesView()) {
-                        Text("Manage Templates")
-                            .frame(maxWidth: .infinity)
-                            .padding()
-                            .background(Color.green)
-                            .foregroundColor(.white)
-                            .cornerRadius(8)
-                    }
-                    
-                    NavigationLink(destination: ManageWorkoutsView()) {
-                        Text("Workout history")
-                            .frame(maxWidth: .infinity)
-                            .padding()
-                            .background(Color.blue)
-                            .foregroundColor(.white)
-                            .cornerRadius(8)
-                    }
-                }.padding()
+                
                 
             }
-            .navigationTitle("Track Workout")
+            .navigationTitle("Start Tracking")
         }
-    }
-
-    private func loadTemplates() {
-        templates = TemplateManager.shared.readTemplates()
     }
 }
